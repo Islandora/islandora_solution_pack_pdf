c258a1669d855419b97b698ab11c1cdb8001453c
jonathangreen <jonathan@discoverygarden.ca>
2012-06-07 13:28:50 -0300

    Initial commit for the 7.x version of PDF.

---
+++ theme/islandora-pdf.tpl.php
@@ -0,0 +1,69 @@
+<?php
+
+/*
+ * islandora-pdf.tpl.php
+ * 
+ *
+ * 
+ * This file is part of Islandora.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with the program.  If not, see <http ://www.gnu.org/licenses/>.
+ */
+?>
+<?php if(isset($islandora_object_label)): ?>
+  <?php drupal_set_title("$islandora_object_label"); ?>
+<?php endif; ?>
+
+<div class="islandora-pdf-object islandora">
+  <div class="islandora-pdf-content-wrapper clearfix">
+    <?php if(isset($islandora_preview_link)): ?>
+      <div class="islandora-pdf-content">
+        <?php print $islandora_preview_link; ?>
+      </div>
+    <?php endif; ?>
+  <div class="islandora-pdf-sidebar">
+    <?php if(isset($dc_array['dc:description']['value'])): ?>
+      <h2><?php print $dc_array['dc:description']['label']; ?></h2>
+      <p><?php print $dc_array['dc:description']['value']; ?></p>
+    <?php endif; ?>
+    <?php if($parent_collections): ?>
+      <div>
+        <h2>In Collections</h2>
+        <ul>
+          <?php foreach($parent_collections as $key => $value): ?>
+            <li><?php print $value['label_link'] ?></li>
+          <?php endforeach; ?>
+        </ul>
+      </div>
+    <?php endif; ?>
+  </div>
+  </div>
+  <fieldset class="collapsible collapsed islandora-pdf-metadata">
+  <legend><span class="fieldset-legend">Extended Details</span></legend>
+    <div class="fieldset-wrapper">
+      <dl class="islandora-inline-metadata islandora-pdf-fields">
+        <?php $row_field = 0; ?>
+        <?php foreach($dc_array as $key => $value): ?>
+          <dt class="<?php print $value['class']; ?><?php print $row_field == 0 ? ' first' : ''; ?>">
+            <?php print $value['label']; ?>
+          </dt>
+          <dd class="<?php print $value['class']; ?><?php print $row_field == 0 ? ' first' : ''; ?>">
+            <?php print $value['value']; ?>
+          </dd>
+          <?php $row_field++; ?>
+        <?php endforeach; ?>
+      </dl>
+    </div>
+  </fieldset>
+</div>
